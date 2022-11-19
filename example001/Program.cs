191dee6af4d1c3996e8ae7cc5e1e418f01ea6f58
dimas360 <bios5524@yandex.ru>
2022-11-19 19:46:56 +0300

    задача 1

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int number = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int number2 = int.Parse(ReadLine());
+
+if (number>number2)
+{
+    WriteLine($"Число {number} является максимальным, а число {number2} минимальным.");
+}
+else
+{
+    WriteLine($"Число {number} является минимальным, а число {number2} максимальным.");
+}
