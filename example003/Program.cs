7984abbadd360bda8c74a0aa898995eb2e6a647c
dimas360 <bios5524@yandex.ru>
2022-11-19 20:22:01 +0300

    задача 3

---
+++ example003/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите любое число: ");
+int number = int.Parse(ReadLine());
+
+int x = number % 2;
+
+if (x == 0)
+    Write($"Число {number} является четным");
+else
+    Write($"Число {number} не является четным");
