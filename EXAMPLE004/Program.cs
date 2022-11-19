76942404dcd11fe4c130a4d21d56f46bbfa5db9d
dimas360 <bios5524@yandex.ru>
2022-11-19 20:30:17 +0300

    задача 4

---
+++ EXAMPLE004/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите любое число: ");
+int N = int.Parse(ReadLine());
+int Z = 2;
+WriteLine($"Ниже будут приведены все четные числа до {N}");
+while(Z<=N)
+{   if(Z%2 == 0)
+    {                                
+       WriteLine(Z);        
+    }
+    Z++;
+}
