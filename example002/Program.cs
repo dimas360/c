997641aac076224887f893d46b820a1a1f151f7b
dimas360 <bios5524@yandex.ru>
2022-11-19 19:56:14 +0300

    задача 2

---
+++ example002/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int number = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int number2 = int.Parse(ReadLine());
+Write("Введите третье число: ");
+int number3 = int.Parse(ReadLine());
+
+if(number > number2 && number >number3)
+    WriteLine($"Число {number} является максимальным.");
+if(number2>number && number2>number3)
+    WriteLine($"Число {number2} является максимальным");
+else
+    WriteLine($"Число {number3} является максимальным");
